16ac492c0dc92ac6dab927924a2ebb4cb44c93e5
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2024-10-06 18:11:45 +0300

    Add TrainerService template

---
+++ src/main/java/edu/java/springcoretask/service/TrainerService.java
@@ -0,0 +1,17 @@
+package edu.java.springcoretask.service;
+
+import edu.java.springcoretask.entity.Trainer;
+
+public class TrainerService {
+    public void create(Trainer trainer) {
+
+    }
+
+    public void update(Trainer trainer) {
+
+    }
+
+    public Trainer select(Trainer trainer){
+        return null;
+    }
+}
