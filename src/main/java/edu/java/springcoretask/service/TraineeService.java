823bdd854bf7baf52b4e41b7c0b1c5cec6c4248c
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2024-10-06 18:02:25 +0300

    Add TraineeService template

---
+++ src/main/java/edu/java/springcoretask/service/TraineeService.java
@@ -0,0 +1,20 @@
+package edu.java.springcoretask.service;
+
+import edu.java.springcoretask.entity.Trainee;
+
+public class TraineeService {
+    public void create(Trainee trainee) {
+
+    }
+
+    public void update(Trainee trainee) {
+
+    }
+
+    public Trainee delete(Trainee trainee) {
+        return null;
+    }
+    public Trainee select(Trainee trainee){
+        return null;
+    }
+}
