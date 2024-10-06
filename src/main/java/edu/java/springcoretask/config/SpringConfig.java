92fb4ffffc6f8d37e5b4b15cf3708b8a54111658
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2024-10-06 17:28:39 +0300

    Add create TrainingStorage bean

--- src/main/java/edu/java/springcoretask/config/SpringConfig.java
+++ src/main/java/edu/java/springcoretask/config/SpringConfig.java
@@ -2,6 +2,7 @@ package edu.java.springcoretask.config;
 
 import edu.java.springcoretask.storage.TraineeStorage;
 import edu.java.springcoretask.storage.TrainerStorage;
+import edu.java.springcoretask.storage.TrainingStorage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -17,5 +18,10 @@ public class SpringConfig {
     @Bean
     public TrainerStorage trainerStorage() {
         return new TrainerStorage();
+    }
+
+    @Bean
+    public TrainingStorage trainingStorage() {
+        return new TrainingStorage();
     }
 }
--- src/main/java/edu/java/springcoretask/storage/TrainingStorage.java
+++ src/main/java/edu/java/springcoretask/storage/TrainingStorage.java
@@ -1,6 +1,16 @@
 package edu.java.springcoretask.storage;
 
 import edu.java.springcoretask.entity.Training;
+import edu.java.springcoretask.utility.JsonToObject;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.annotation.PostConstruct;
 
 public class TrainingStorage extends Storage<Training>{
+    @Value("${training_storage_path}")
+    private String path;
+    @PostConstruct
+    public void init() {
+        setStorage(JsonToObject.getTrainingMapFromJson(path, Training.class));
+    }
 }
