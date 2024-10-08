421736e293e897c88a9caba910934cacdd6e454f
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2024-10-08 17:27:22 +0300

    Add loger to TraineeService

--- src/main/java/edu/java/springcoretask/service/TraineeService.java
+++ src/main/java/edu/java/springcoretask/service/TraineeService.java
@@ -3,25 +3,31 @@ package edu.java.springcoretask.service;
 import edu.java.springcoretask.dao.TraineeDAO;
 import edu.java.springcoretask.entity.Trainee;
 import edu.java.springcoretask.utility.PasswordGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TraineeService {
     @Autowired
     private TraineeDAO traineeDAO;
-
+    private static Logger logger = LoggerFactory.getLogger(TraineeService.class);
     public void create(Trainee trainee) {
         trainee.setUserName(createValidUserName(trainee));
         trainee.setPassword(PasswordGenerator.generatePassword());
         trainee.setIsActive(true);
         traineeDAO.create(trainee);
+        logger.info("Trainee created with di {}", trainee.getId());
     }
 
     public void update(Trainee trainee) {
         traineeDAO.update(trainee);
+        logger.info("Trainee updated with di {}", trainee.getId());
     }
 
     public Trainee delete(Trainee trainee) {
-        return traineeDAO.delete(trainee);
+        Trainee deletedTrainee = traineeDAO.delete(trainee);
+        logger.info("Trainee deleted with di {}", deletedTrainee.getId());
+        return deletedTrainee;
     }
 
     public Trainee select(String userName) {
