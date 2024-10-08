1f7e82f23e582df783b926860958879d2cc1c35c
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2024-10-08 22:29:53 +0300

    Add JUnit dependency

---
+++ src/test/java/edu/java/springcoretask/service/TraineeServiceTest.java
@@ -0,0 +1,12 @@
+package edu.java.springcoretask.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TraineeServiceTest {
+
+    @Test
+    void create() {
+    }
+}
