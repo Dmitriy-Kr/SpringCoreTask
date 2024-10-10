c1493268fc893f2d2a9108f88f275c820da6a82b
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2024-10-10 21:44:50 +0300

    Add Trainer service tests

--- src/test/java/edu/java/springcoretask/service/TrainerServiceTest.java
+++ src/test/java/edu/java/springcoretask/service/TrainerServiceTest.java
@@ -76,4 +76,80 @@ class TrainerServiceTest {
         assertEquals(new TrainingType("yoga"), captor.getValue().getSpecialization());
 
     }
+
+    @Test
+    void givenTrainerHasDifferentFirstName_whenUpdate_shouldChangeUserName() {
+
+        Trainer updatedTrainer = new Trainer(32L, "Denis", "Ivanov",
+                "Igor.Ivanov", "aaahj5yto9", false, new TrainingType("martial arts"));
+
+        Mockito.when(trainerDAO.select(anyString()))
+                .thenReturn(Optional.of(new Trainer(32L, "Igor", "Ivanov",
+                        "Igor.Ivanov", "12ghj5yto8", true, new TrainingType("yoga"))))
+                .thenReturn(Optional.empty());
+
+        trainerService.update(updatedTrainer);
+
+        final ArgumentCaptor<Trainer> captor = ArgumentCaptor.forClass(Trainer.class);
+
+        verify(trainerDAO, times(1)).update(captor.capture());
+
+        assertFalse(captor.getValue().isActive());
+
+        assertEquals(new TrainingType("martial arts"), captor.getValue().getSpecialization());
+
+        assertEquals("Denis.Ivanov", captor.getValue().getUserName());
+
+    }
+
+    @Test
+    void givenTrainerHasDifferentLastName_whenUpdate_shouldChangeUserName() {
+
+        Trainer updatedTrainer = new Trainer(32L, "Igor", "Schmidt",
+                "Igor.Ivanov", "aaahj5yto9", false, new TrainingType("martial arts"));
+
+        Mockito.when(trainerDAO.select(anyString()))
+                .thenReturn(Optional.of(new Trainer(32L, "Igor", "Ivanov",
+                        "Igor.Ivanov", "12ghj5yto8", true, new TrainingType("yoga"))))
+                .thenReturn(Optional.empty());
+
+        trainerService.update(updatedTrainer);
+
+        final ArgumentCaptor<Trainer> captor = ArgumentCaptor.forClass(Trainer.class);
+
+        verify(trainerDAO, times(1)).update(captor.capture());
+
+        assertFalse(captor.getValue().isActive());
+
+        assertEquals(new TrainingType("martial arts"), captor.getValue().getSpecialization());
+
+        assertEquals("Igor.Schmidt", captor.getValue().getUserName());
+
+    }
+
+    @Test
+    void select_findTrainer_returnTrainer(){
+
+        Trainer checkTrainer = new Trainer(32L, "Igor", "Ivanov",
+                "Igor.Ivanov", "12ghj5yto8", true, new TrainingType("martial arts"));
+
+        Mockito.when(trainerDAO.select(anyString()))
+                .thenReturn(Optional.of(new Trainer(32L, "Igor", "Ivanov",
+                        "Igor.Ivanov", "12ghj5yto8", true, new TrainingType("martial arts"))));
+
+        Trainer foundTrainer = trainerService.select("Igor.Ivanov");
+
+        assertEquals(checkTrainer, foundTrainer);
+    }
+
+    @Test
+    void select_notFindTrainer_returnTrainerWithNegativeId(){
+
+        Mockito.when(trainerDAO.select(anyString()))
+                .thenReturn(Optional.empty());
+
+        Trainer foundTrainer = trainerService.select("Igor.Ivanov");
+
+        assertTrue(foundTrainer.getId() < 0);
+    }
 }
