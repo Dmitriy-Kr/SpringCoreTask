4e349ddd51e773bc604b5857cc1adb675fd52f1e
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2024-10-10 18:42:42 +0300

    Add testUpdate method

--- src/test/java/edu/java/springcoretask/service/TraineeServiceTest.java
+++ src/test/java/edu/java/springcoretask/service/TraineeServiceTest.java
@@ -5,33 +5,72 @@ import edu.java.springcoretask.entity.Trainee;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 @ExtendWith(MockitoExtension.class)
 class TraineeServiceTest {
     @InjectMocks
     private TraineeService traineeService;
     @Mock
     private TraineeDAO traineeDAO;
-    private Trainee trainee = new Trainee("Igor","Ivanov", LocalDate.parse("1999-10-05"),"Simonova str 7/15");
+    private Trainee trainee = new Trainee("Igor", "Ivanov", LocalDate.parse("1999-10-05"), "Simonova str 7/15");
+
     @Test
     void testCreate() {
-    Mockito.when(traineeDAO.create(trainee)).thenReturn(true);
-    Mockito.when(traineeDAO.select(ArgumentMatchers.anyString()))
-            .thenReturn(Optional.ofNullable(trainee))
-            .thenReturn(Optional.ofNullable(trainee))
-            .thenReturn(Optional.ofNullable(trainee))
-            .thenReturn(Optional.empty());
+        Mockito.when(traineeDAO.create(trainee)).thenReturn(true);
+        Mockito.when(traineeDAO.select(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.ofNullable(trainee))
+                .thenReturn(Optional.ofNullable(trainee))
+                .thenReturn(Optional.ofNullable(trainee))
+                .thenReturn(Optional.empty());
 
-    traineeService.create(trainee);
+        traineeService.create(trainee);
 
-        Assertions.assertEquals("Igor.Ivanov2", trainee.getUserName());
-        Assertions.assertEquals(10, trainee.getPassword().length());
+        assertEquals("Igor.Ivanov2", trainee.getUserName());
+        assertEquals(10, trainee.getPassword().length());
+    }
+
+    @Test
+    void noTraineeFounded_whenUpdate_shouldThrowException() {
+        Mockito.when(traineeDAO.select(ArgumentMatchers.anyString())).thenReturn(Optional.empty());
+        assertThrows(RuntimeException.class, () -> traineeService.update(trainee));
+    }
+
+    @Test
+    void testUpdate() {
+        Trainee updatedTrainee = new Trainee(32L, "Igor", "Ivanov",
+                "Igor.Ivanov", "aaahj5yto9", false, LocalDate.parse("1999-10-05"), "Simonova str 7/15");
+
+        Mockito.when(traineeDAO.select(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.of(new Trainee(32L, "Igor", "Ivanov",
+                        "Igor.Ivanov", "12ghj5yto8", true, LocalDate.parse("1999-10-05"), "West Baker str 31")));
+
+        traineeService.update(updatedTrainee);
+
+        final ArgumentCaptor<Trainee> captor = ArgumentCaptor.forClass(Trainee.class);
+
+        verify(traineeDAO, times(1)).update(captor.capture());
+
+        assertFalse(captor.getValue().isActive());
+        assertEquals("Simonova str 7/15", captor.getValue().getAddress());
+    }
+
+    @Test
+    void givenTraineeHasDifferentFirstName_whenUpdate_shouldChangeUserName() {
+        Mockito.when(traineeDAO.select(ArgumentMatchers.anyString()))
+                .thenReturn(Optional.of(new Trainee(32L, "Ivan", "Ivanov",
+                        "Ivan.Ivanov", "12ghj5yto8", true, LocalDate.parse("1999-10-05"), "Simonova str 7/15")));
+
+        traineeService.update(trainee);
+
+
     }
 }
